418350b71ac172c87da8c5a10226f851b418c821
Rakib Linux <rakibulinux@gmail.com>
2023-03-31 11:37:55 +0600

    added item for wiki api

---
+++ app/components/Search.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+
+export default function Search() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch("");
+    router.push(`/${search}/`);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="w-50 flex justify-center md:justify-between"
+    >
+      <input
+        type="text"
+        name=""
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search"
+      />
+      <button className="p-2 text-xl rounded-xl bg-slate-300 ml-2 font-bold">
+        🚀
+      </button>
+    </form>
+  );
+}
